cf64324ebebd12c09d745e9d65a2d98ba2562f29
Bluewy <bluewyechache@gmail.com>
2021-01-14 15:10:32 +0100

    Create info.py

---
+++ cogs/info.py
@@ -0,0 +1,17 @@
+import discord, config, time
+
+
+
+class info(commands.Cog, name="Info"):
+
+    @commands.command(brief="Bot's latency to discord")
+    async def ping(self, ctx):
+        """ See bot's latency to discord """
+        discord_start = time.monotonic()
+        async with self.bot.session.get("https://discord.com/") as resp:
+            if resp.status == 200:
+                discord_end = time.monotonic()
+                discord_ms = f"{round((discord_end - discord_start) * 1000)}ms"
+            else:
+                discord_ms = "fucking dead"
+        await ctx.send(f"\U0001f3d3 Pong   |   {discord_ms}")
