4e7ffbfabb76a5b0ee8cb4cbf4f8ef9872803e16
Bluewy <bluewyechache@gmail.com>
2021-01-30 15:13:11 +0100

    Update info.py

--- cogs/info.py
+++ cogs/info.py
@@ -6,18 +6,18 @@ class info(commands.Cog, name="Info"):
   def __init__(self, bot):
     self.bot = bot
 
-    #@commands.command(brief="Bot's latency to discord")
-    #async def ping(self, ctx):
-    #    """ See bot's latency to discord """
-    #    discord_start = time.monotonic()
-    #    bot.session = aiohttp.ClientSession(loop=bot.loop)
-    #    async with self.bot.session.get("https://discord.com/") as resp:
-    #        if resp.status == 200:
-    #            discord_end = time.monotonic()
-    #            discord_ms = f"{round((discord_end - discord_start) * 1000)}ms"
-    #        else:
-    #            discord_ms = "fucking dead"
-    #    await ctx.send(f"\U0001f3d3 Pong   |   {discord_ms}")
+    @commands.command(brief="Bot's latency to discord")
+    async def ping(self, ctx):
+        """ See bot's latency to discord """
+        discord_start = time.monotonic()
+        bot.session = aiohttp.ClientSession(loop=bot.loop)
+        async with self.bot.session.get("https://discord.com/") as resp:
+            if resp.status == 200:
+                discord_end = time.monotonic()
+                discord_ms = f"{round((discord_end - discord_start) * 1000)}ms"
+            else:
+                discord_ms = "fucking dead"
+        await ctx.send(f"\U0001f3d3 Pong   |   {discord_ms}")
 
     @commands.command(brief="test command")
     async def respond(self, ctx):
