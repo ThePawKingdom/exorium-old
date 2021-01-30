ac56a5dc574dcceb5fcc5cde159b086eb3deb3df
Bluewy <bluewyechache@gmail.com>
2021-01-30 14:52:24 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,11 +19,11 @@ async def run():
 
 bot = commands.Bot(command_prefix=get_prefix, case_insensitive =True, allowed_mentions =discord.AllowedMentions.none(), max_messages=10000)
 
-        for extension in config.EXTENSIONS:
-            try:
-                self.load_extension(extension)
-                print(f'[EXTENSION] {extension} was loaded successfully!')
-            except Exception as e:
-                tb = traceback.format_exception(type(e), e, e.__traceback__) 
-                tbe = "".join(tb) + ""
-                print(f'[WARNING] Could not load extension {extension}: {tbe}')
+for extension in config.extensions:
+    try:
+        self.load_extension(extension)
+        print(f'[extension] {extension} was loaded successfully!')
+    except Exception as e:
+        tb = traceback.format_exception(type(e), e, e.__traceback__)
+        tbe = "".join(tb) + ""
+        print(f'[WARNING] Could not load extension {extension}: {tbe}')
